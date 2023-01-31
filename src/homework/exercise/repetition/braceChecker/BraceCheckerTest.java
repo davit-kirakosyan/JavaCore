84670d5cc3645973febb87d73d00ac10bdef0d5b
david-kirakosyan <davidkirakosyan504@gmail.com>
2023-01-31 22:53:11 +0400

    repetition/braceChecker

---
+++ src/homework/exercise/repetition/braceChecker/BraceCheckerTest.java
@@ -0,0 +1,11 @@
+package homework.exercise.repetition.braceChecker;
+
+
+public class BraceCheckerTest {
+
+    public static void main(String[] args) {
+
+        BraceChecker braceChecker = new BraceChecker("this[(((((((((((((((({{{{{{{{{{{{{{{{{{{{{{{{{{{{{{ is })a jav]]]]]]]]]]]]]]]]]a");
+        braceChecker.check();
+    }
+}
